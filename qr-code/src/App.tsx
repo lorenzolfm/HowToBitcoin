ec62856aaf8c6fdf9ced8fcc6429d9a4f03db88e
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-11-20 18:54:52 -0300

    feat: working

--- qr-code/src/App.tsx
+++ qr-code/src/App.tsx
@@ -1,24 +1,41 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const App = () => {
-  const [ammount, setAmmount] = useState(0);
+  const [amount, setAmount] = useState('0');
   const [address, setAddress] = useState('');
+  const [data, setData] = useState('');
+  const [qrCode, setQrCode] = useState('');
+
+  useEffect(() => {
+    setQrCode(`http://api.qrserver.com/v1/create-qr-code/?data=${data}`)
+  }, [data]);
+
+  const handleClick = () => {
+    const str = `bitcoin:${address}?amount=${amount}`;
+    setData(str);
+  }
 
   return (
     <div className="app">
-      <h1>QR Code Generator</h1>
-      <input
-        placeholder="ammount"
-        type="text"
-        value={ammount === 0 ? '' : ammount}
-        onChange={(e) => setAmmount(parseFloat(e.target.value))}
-      />
-      <input
-        placeholder="address"
-        type="text"
-        value={address}
-        onChange={(e) => setAddress(e.target.value)}
-      />
+      <div>
+        <h1>QR Code Generator</h1>
+        <input
+          placeholder="amount"
+          type="text"
+          value={amount === '0' ? '' : amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <input
+          placeholder="address"
+          type="text"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+        <button onClick={handleClick}>Generate</button>
+      </div>
+      <div>
+        <img src={qrCode} alt="" />
+      </div>
     </div>
   );
 };
