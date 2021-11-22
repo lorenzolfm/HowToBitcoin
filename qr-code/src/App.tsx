419a16bd719246a9449bf277fd52b9dd4fd5ada8
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-11-22 08:42:26 -0300

    refactor: using input component

--- qr-code/src/App.tsx
+++ qr-code/src/App.tsx
@@ -1,41 +1,61 @@
 import { useState, useEffect } from 'react';
 import { QrCode } from './QrCode';
+import { Input } from './Input';
 
 export const App = () => {
-  const [amount, setAmount] = useState('0');
+  const [amount, setAmount] = useState('');
   const [address, setAddress] = useState('');
   const [data, setData] = useState('');
+  const [displayErrorMsg, setDisplayErrorMsg] = useState(false);
   const [qrCode, setQrCode] = useState('');
 
   useEffect(() => {
-    if (data !== '') 
+    if (data !== '')
       setQrCode(`http://api.qrserver.com/v1/create-qr-code/?data=${data}`)
   }, [data]);
 
   const handleClick = () => {
-    const str = `bitcoin:${address}?amount=${amount}`;
-    setData(str);
+    if (amount !== '' && address !== '') {
+      if (displayErrorMsg) setDisplayErrorMsg(false);
+
+      const str = `bitcoin:${address}?amount=${amount}`;
+      setData(str);
+    } else {
+      setDisplayErrorMsg(true);
+    }
   }
 
   return (
-    <div className="app">
+    <div className="ui container">
       <div>
-        <h1>QR Code Generator</h1>
-        <input
-          placeholder="amount"
-          type="text"
-          value={amount === '0' ? '' : amount}
-          onChange={(e) => setAmount(e.target.value)}
-        />
-        <input
-          placeholder="address"
-          type="text"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-        />
-        <button onClick={handleClick}>Generate</button>
+        <div className="ui segment" style={{ marginTop: 20 }}>
+          <h1 style={{ textAlign: 'center' }}>
+            QR Code Generator
+          </h1>
+          <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+          }}>
+            { displayErrorMsg &&
+              <p>You should enter an amount and an address!</p>
+            }
+            <Input
+              placeholder="Amount"
+              value={amount}
+              onChange={setAmount}
+            />
+            <Input
+              placeholder="Address"
+              value={address}
+              onChange={setAddress}
+            />
+            <button className="ui primary basic button" onClick={handleClick}>
+              Generate
+            </button>
+          </div>
+          <QrCode url={qrCode} />
+        </div>
       </div>
-      <QrCode url={qrCode} />
     </div>
   );
 };
