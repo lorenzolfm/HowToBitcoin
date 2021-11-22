917a0b4dcf91cd3220c1e8327ae9937f1e4c3f6f
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-11-22 08:36:15 -0300

    refactor: rendering loader when no input

--- qr-code/src/QrCode.tsx
+++ qr-code/src/QrCode.tsx
@@ -3,9 +3,34 @@ type QrCodeProps = {
 };
 
 export const QrCode = ({ url }: QrCodeProps) => {
+  const renderLoader = () => {
+    return (
+      <p style={{ alignSelf: 'center' }}>Waiting for user input</p>
+    );
+  };
+
+  const renderQrCode = () => {
+    return (
+      <img
+        src={url}
+        alt=""
+        style={{
+          width: 400,
+          height: 400,
+          alignSelf: 'center',
+        }}
+      />
+    );
+  }
+
   return (
-    <div className="qrcode">
-      <img src={url} alt="" />
+    <div className="ui segment qrcode" style={{
+      display: 'flex',
+      flexDirection: 'column',
+    }}>
+      <h2 style={{ textAlign: 'center' }}>QR Code:</h2>
+      {url === '' ?
+        renderLoader() : renderQrCode()}
     </div>
   );
-};
+}
