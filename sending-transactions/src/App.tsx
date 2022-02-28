44c742a66750d9736c26781d576e0b3d5d97fa1e
Lorenzo Maturano <maturanolorenzo@gmail.com>
2022-02-28 08:59:45 -0300

    feat: hashing input

--- sending-transactions/src/App.tsx
+++ sending-transactions/src/App.tsx
@@ -1,5 +1,29 @@
+import { sha256 } from 'js-sha256';
+import { useState } from 'react';
+
 export const App = (): JSX.Element => {
+  const [shaInput, setShaInput] = useState<string>('')
+
+  const hash = () => {
+    return sha256(shaInput);
+  }
+
   return (
-    <h1>Hello from App</h1>
+    <>
+      <h3 className="ui center aligned header">Hello from App</h3>
+      <div className="ui container">
+        <div className="ui segments">
+          <div className="ui segment">
+            SHA-256
+            <input
+              type="text"
+              value={shaInput}
+              onChange={(e) => setShaInput(e.target.value)}
+            />
+            <p>{shaInput && hash()}</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
